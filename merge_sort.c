fcc49b06794c5bf0e02e8f73ea751cdf18ecdbd1
Manoj <eerabattinimanoj1@gmail.com>
2025-03-23 19:02:28 +0530

    Initial commit

---
+++ merge_sort.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<stdlib.h>
+void merge(int arr[], int l, int m, int r) {
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+    int L[n1], R[n2];
+    for(i = 0; i < n1; i++) {
+        L[i] = arr[l + i];
+    }
+    for(i = 0; i < n2; i++) {
+        R[i] = arr[m + 1 + i];
+    }
+    i = 0; j = 0, k = l;
+    while(i < n1 && j < n2) {
+        if(L[i] < R[j]) {
+            arr[k] = L[i]; i++;
+        } else {
+            arr[k] = R[j]; j++;
+        }
+        k++;
+    }
+    while(i < n1) { arr[k++] = L[i++]; }
+    while(j < n2) { arr[k++] = R[j++]; }
+}
+void mergeSort(int arr[], int l, int r) {
+    if(l < r) {
+        int m = (l + r) / 2;
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+        merge(arr, l, m, r);
+    }
+}
+int main() {
+    int arr[] = {4, 24, 26, 2, 5, 43};
+    int size = sizeof(arr) / sizeof(int);
+    mergeSort(arr, 0, size - 1);
+    for(int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
